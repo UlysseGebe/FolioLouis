125dd69e18a9ba36f082860aca618e40baf57d74
UlysseGebe <Uly35534e31>
2019-12-25 01:52:22 +0100

    loader

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import Slider from './components/Slider/Slider';
-// import Loader from './components/Loader/Loader';
+import Loader from './components/Loader/Loader';
 import Header from './components/Header/Header';
 import './App.css';
 
+const loader = () => {
+  let perfData = window.performance.timing, // The PerformanceTiming interface represents timing-related performance information for the given page.
+  EstimatedTime = -(perfData.loadEventEnd - perfData.navigationStart),
+  time = parseInt((EstimatedTime / 1000) % 60) * 100;
+  setTimeout(() => {
+    document.querySelector('#Loader').classList.add('remove')
+    setTimeout(() => {
+      document.querySelector('#Loader').outerHTML = ""
+     }, 500);
+   }, time);
+}
+
+loader()
+
 function App() {
+
   return (
     <div className="App">
       <Header />
-      {/* <Loader /> */}
+      <Loader/>
       <Slider/>
     </div>
   );
